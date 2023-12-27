f24e6e87e8c3cce1490af18665ac8cf237f3ae24
Alon Cory <alon.cory@runi.ac.il>
2023-12-27 16:51:47 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,11 +5,12 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]); // Gets the size of board to print
 		for (int i = 0; i < n; i++) {
-			if (i % 2 != 0) {
-				System.out.print(" "); // Prints a backspace
-			}
 			for (int j = 0; j < n; j++) {
-				System.out.print("* "); // Prints n times in row the string "* "
+				if (i % 2 == 0) {
+					System.out.print("* "); // Prints n times in row the string "* "
+				} else {
+					System.out.print(" *"); // Prints n times in row the string " *"
+				}
 			}
 			System.out.println(); // Skips to the next line
 		}
